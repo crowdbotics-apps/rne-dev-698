2fad469e91bbad9ed7b7c48aecd3fcaff05278a2
Crowdbotics <team@crowdbotics.com>
2019-02-22 03:11:22 +0000

    initial commit

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: waitress-serve --port=$PORT backend.rne_dev_698.wsgi:application
+release: ./backend/release-tasks.sh
---
+++ backend/rne_dev_698/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for rne_dev_698 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rne_dev_698.settings")
+
+application = get_wsgi_application()
